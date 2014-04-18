b1326709d18f6277d354733c1e45c0d2506e3af1
Jill Adams <jill@majorrobot.com>
2014-04-17 23:41:27 -0400

    initial commit

---
+++ wp-content/plugins/amazon-auto-links/class_final/AmazonAutoLinks_Debug.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Methods used for debugging
+ * 
+ * @package     Amazon Auto Links
+ * @copyright   Copyright (c) 2013, Michael Uno
+ * @authorurl	http://michaeluno.jp
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since		1.0.0
+ * 
+*/
+
+final class AmazonAutoLinks_Debug {
+
+	static public function dumpArray( $arr, $strFilePath=null ) {
+		
+		if ( ! defined( 'WP_DEBUG' ) || ! WP_DEBUG ) return;
+		
+		echo self::getArray( $arr, $strFilePath );
+		
+	}
+
+	static public function getArray( $arr, $strFilePath=null ) {
+		
+		if ( ! defined( 'WP_DEBUG' ) || ! WP_DEBUG ) return;
+		
+		if ( $strFilePath ) 
+			self::logArray( $arr, $strFilePath );			
+			
+		// esc_html() has a bug that breaks with complex HTML code.
+		return "<div><pre class='dump-array'>" . htmlspecialchars( print_r( $arr, true ) ) . "</pre><div>";	
+		
+	}
+	
+	static public function logArray( $asArray, $sFilePath=null ) {
+		
+		if ( ! defined( 'WP_DEBUG' ) || ! WP_DEBUG ) return;
+			
+		static $_iPageLoadID;
+		$_iPageLoadID = $_iPageLoadID ? $_iPageLoadID : uniqid();		
+		
+		$oCallerInfo = debug_backtrace();
+		$sCallerFunction = $oCallerInfo[ 1 ]['function'];
+		$sCallerClasss = $oCallerInfo[ 1 ]['class'];
+		file_put_contents( 
+			$sFilePath ? $sFilePath : dirname( __FILE__ ) . '/array_log.txt', 
+			date( "Y/m/d H:i:s", current_time( 'timestamp' ) ) . ' ' . "{$_iPageLoadID} {$sCallerClasss}::{$sCallerFunction} " . self::getCurrentURL() . PHP_EOL	
+			. print_r( $asArray, true ) . PHP_EOL . PHP_EOL,
+			FILE_APPEND 
+		);					
+							
+	}	
+
+	/**
+	 * Retrieves the currently loaded page url.
+	 * 
+	 * @since			1.3.3.11
+	 */
+	static public function getCurrentURL() {
+		$sSSL = ( !empty( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] == 'on' ) ? true:false;
+		$sServerProtocol = strtolower( $_SERVER['SERVER_PROTOCOL'] );
+		$sProtocol = substr( $sServerProtocol, 0, strpos( $sServerProtocol, '/' ) ) . ( ( $sSSL ) ? 's' : '' );
+		$sPort = $_SERVER['SERVER_PORT'];
+		$sPort = ( ( !$sSSL && $sPort=='80' ) || ( $sSSL && $sPort=='443' ) ) ? '' : ':' . $sPort;
+		$sHost = isset( $_SERVER['HTTP_X_FORWARDED_HOST'] ) ? $_SERVER['HTTP_X_FORWARDED_HOST'] : isset( $_SERVER['HTTP_HOST'] ) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME'];
+		return $sProtocol . '://' . $sHost . $sPort . $_SERVER['REQUEST_URI'];
+	}
+	
+	static public function echoMemoryUsage() {
+		
+		if ( ! defined( 'WP_DEBUG' ) || ! WP_DEBUG ) return;
+				   
+		echo self::getMemoryUsage() . "<br/>";
+		
+	} 		
+
+    static public function getMemoryUsage( $intType=1 ) {	// since 1.1.4
+       
+		if ( ! defined( 'WP_DEBUG' ) || ! WP_DEBUG ) return;
+	   
+		$intMemoryUsage = $intType == 1 ? memory_get_usage( true ) : memory_get_peak_usage( true );
+       
+        if ( $intMemoryUsage < 1024 ) return $intMemoryUsage . " bytes";
+        
+		if ( $intMemoryUsage < 1048576 ) return round( $intMemoryUsage/1024,2 ) . " kilobytes";
+        
+        return round( $intMemoryUsage / 1048576,2 ) . " megabytes";
+           
+    } 		
+	
+	static public function getOption( $strKey ) {
+
+		if ( ! defined( 'WP_DEBUG' ) || ! WP_DEBUG ) return;
+		
+		$oOption = & $GLOBALS['oAmazonAutoLinks_Option'];		
+		if ( ! isset( $oOption->arrOptions[ $strKey ] ) ) return;
+		
+		die( self::getArray( $oOption->arrOptions[ $strKey ] ) );
+		
+	}
+}
